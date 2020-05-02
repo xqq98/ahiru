3f2a95f09e48c4f4c6137944078014aa1cd0522a
guoshilong <shilong.guo@pactera.com>
2020-05-02 20:15:49 +0800

    test

--- src/main/java/com/webservice/ahiru/service/UPEmpDtlService.java
+++ src/main/java/com/webservice/ahiru/service/UPEmpDtlService.java
@@ -2,6 +2,7 @@ package com.webservice.ahiru.service;
 
 import com.baomidou.mybatisplus.service.IService;
 import com.webservice.ahiru.entity.UPEmpDtl;
+import com.webservice.ahiru.pojo.Result;
 
 /**
  * <p>
@@ -13,5 +14,12 @@ import com.webservice.ahiru.entity.UPEmpDtl;
  */
 //Service层业务接口类编写
 public interface UPEmpDtlService extends IService<UPEmpDtl> {
+    //获取数据库表（M_EMP_DTL）的数据，以list列表的形式，把查询出来的数据保存在数据对象中
+    Result getInfo(UPEmpDtl upEmpDtl);
 
+    //更新表（M_EMP_DTL）
+    Result UpdateEmpInfo(UPEmpDtl uPEmpDtl);
+
+    //更新表（M_EMP_DTL）
+    Result UpdateBpInfo(UPEmpDtl upEmpDtl);
 }
--- src/main/java/com/webservice/ahiru/service/impl/SEVEmpListServiceImpl.java
+++ src/main/java/com/webservice/ahiru/service/impl/SEVEmpListServiceImpl.java
@@ -2,6 +2,7 @@ package com.webservice.ahiru.service.impl;
 
 import com.webservice.ahiru.entity.SEVEmpList;
 import com.webservice.ahiru.mapper.SEVEmpListMapper;
+import com.webservice.ahiru.pojo.Result;
 import com.webservice.ahiru.service.SEVEmpListService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,16 +34,30 @@ public class SEVEmpListServiceImpl implements SEVEmpListService {
     @Override
     //获取视图（V_PM_CHOSEN）的数据，以list列表的形式，把查询出来的数据保存在数据对象中（根据三个主键），返回resultPmChosen
 //    public List<SEVEmpList> getInFo(SEVEmpList svl) {
-    public List<SEVEmpList> getInFo(String startDt,String endDt) {
-        List<SEVEmpList> resultSEVEmpList = seVEmpListMapper.getInfoX(startDt,endDt);
+    public Result getInFo(String startDt, String endDt) {
+        Result resul = null;
+        try {
+            List<SEVEmpList> result = seVEmpListMapper.getInfoX(startDt,endDt);
+            resul = new Result(result);
+        } catch (Exception e) {
+            resul = Result.error((e.getMessage()));
+            logger.info("数据异常"+e.getMessage());
+        }
 
-        return resultSEVEmpList;
+        return resul;
     }
 
-    public List<SEVEmpList> getInfoAll(SEVEmpList sevEmpList) {
-        List<SEVEmpList> resultSEVEmpLists = seVEmpListMapper.getInfoAll(sevEmpList);
+    public Result getInfoAll(SEVEmpList sevEmpList) {
+        Result resul = null;
+        try {
+            List<SEVEmpList> result = seVEmpListMapper.getInfoAll(sevEmpList);
+            resul = new Result(result);
+        } catch (Exception e) {
+            resul = Result.error((e.getMessage()));
+            logger.info("数据异常"+e.getMessage());
+        }
 
-        return resultSEVEmpLists;
+        return resul;
     }
 
 }
