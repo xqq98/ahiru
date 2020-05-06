d4e68e36c4339cb6ec1dac55d26997330f1a7a8f
suhongye77 <64140787+suhongye77@users.noreply.github.com>
2020-05-06 22:37:10 +0800

    WangEnji update

--- src/main/java/com/webservice/ahiru/mapper/EmployeeWorkMapper.java
+++ src/main/java/com/webservice/ahiru/mapper/EmployeeWorkMapper.java
@@ -1,6 +1,5 @@
 package com.webservice.ahiru.mapper;
 
-import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.webservice.ahiru.entity.EmployeeWork;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -23,21 +22,14 @@ import java.util.List;
 //数据库访问接口类
 public interface EmployeeWorkMapper {
 
-    // 查询数据
-    List<EmployeeWork> getEmployeeWorkInfo(EmployeeWork employeeWork);
+    // 明细数据取得
+    List<EmployeeWork> getEmployeeWorkDetail(EmployeeWork employeeWork);
 
     // 删除数据
-    //int delEmployeeWorkInfo(List employeeWorkList);
+    int delEmployeeWorkInfo(List employeeWorkList);
 
-    // 更新数据
-    int uptEmployeeWorkInfo(List employeeWorkList);
-
-    //插入数据
-    //int insEmployeeWorkInfo(List employeeWorkList);
-
-    List<EmployeeWork> getEmployeeWorkInfoAll(String startDt, String endDt);
-
-    List<EmployeeWork> getEmployeeWorkInfoAllList(EmployeeWork employeeWorList);
+    // 插入数据
+    int insEmployeeWorkInfo(List employeeWorkList);
 }
 
 
