c276c43d32683b28dd2a97070d1bb67e0c26cfc7
suhongye77 <hongye.su@pactera.com>
2020-07-07 16:39:54 +0800

    认证功能追加

---
+++ src/main/java/com/webservice/ahiru/handler/ServiceInterceptor.java
@@ -0,0 +1,89 @@
+package com.webservice.ahiru.handler;
+
+import com.alibaba.druid.util.StringUtils;
+import com.alibaba.fastjson.JSONObject;
+import com.webservice.ahiru.common.RedisUtil;
+import com.webservice.ahiru.common.UserUtil;
+import com.webservice.ahiru.controller.EmployeeWorkController;
+import com.webservice.ahiru.entity.MEmpDtl;
+import com.webservice.ahiru.exception.AhiruException;
+import com.webservice.ahiru.pojo.Result;
+import com.webservice.ahiru.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@Component
+public class ServiceInterceptor implements HandlerInterceptor {
+
+    private String USERNAME = "USERNAME";
+    private String WECHARTID = "OPENID";
+    private long EXPIRE_TEIM = 3600;
+
+    //Log文件的获取
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private UserService userService;
+
+    @Resource
+    private RedisUtil redisUtil;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        System.out.println("开始拦截........." + request.getRequestURL());
+        //System.out.println("aaa==="+redisUtil.get("aaa"));
+        String loginUser = request.getHeader(USERNAME);
+        String openid = request.getHeader(WECHARTID);
+        PrintWriter writer=null;
+        try {
+            if (loginUser == null || loginUser.equals("")) {
+                throw new Exception("无权限访问资源");
+            }
+
+            if (openid == null || openid.equals("")) {
+                throw new Exception("无权限访问资源");
+            }
+
+            String resOpenid = (String) redisUtil.get(loginUser);
+            if (StringUtils.isEmpty(resOpenid)){
+                MEmpDtl emp = userService.getUserInfo(loginUser);
+                if (emp != null && openid.equals(emp.getWeChatId())) {
+                    redisUtil.set(loginUser,openid,EXPIRE_TEIM);
+                    return true;
+                } else {
+                    throw new Exception("无权限访问资源");
+
+                }
+            }else if (openid.equals(resOpenid)){
+                return true;
+            }else{
+                throw new Exception("无权限访问资源");
+            }
+        } catch (Exception ex) {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json;charset=UTF-8");
+            writer = response.getWriter();
+
+            Result result = Result.error(ex.getMessage());
+            String strResult = JSONObject.toJSONString(result);
+            writer.print(strResult);
+            writer.flush();
+            logger.error("response error", ex);
+            return false;
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+}
+
