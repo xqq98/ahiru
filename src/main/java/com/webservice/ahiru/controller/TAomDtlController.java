2376b00a2c8c94e44f8566d5c4df02909d5fb977
qingfeng522 <guangchen.han@pactera.com>
2020-07-13 14:32:05 +0800

    add

---
+++ src/main/java/com/webservice/ahiru/controller/TAomDtlController.java
@@ -0,0 +1,30 @@
+package com.webservice.ahiru.controller;
+import com.webservice.ahiru.entity.TAomDtl;
+import com.webservice.ahiru.entity.TAomDtlPMItem;
+import com.webservice.ahiru.entity.VAomDtl;
+import com.webservice.ahiru.pojo.Result;
+import com.webservice.ahiru.service.TAomDtlService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/taomdtl")
+public class TAomDtlController {
+    //Log文件的获取
+    private Logger logger = LoggerFactory.getLogger(MProjectController.class);
+    //Service接口的实现类
+    @Autowired
+    private TAomDtlService tAomDtlService;
+
+    //控制器处理“/gettempwork”的URL请求，POST请求
+    @RequestMapping(value = "/gettaomsinglemonth", method = RequestMethod.GET)
+    public Result getTEmpWork() {
+        LinkedHashMap<String, List<TAomDtlPMItem>> result =tAomDtlService.getTAomSingleMonth();
+
+        return Result.ok(result);
+    }
+}
