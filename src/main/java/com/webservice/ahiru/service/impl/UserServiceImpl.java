88352b4158200c7b22b8429d69e694428b01def4
suhongye77 <hongye.su@pactera.com>
2020-07-14 17:05:01 +0800

    后台认证对应

--- src/main/java/com/webservice/ahiru/service/impl/UserServiceImpl.java
+++ src/main/java/com/webservice/ahiru/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.webservice.ahiru.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,9 +19,11 @@ public class UserServiceImpl implements UserService {
     //Log文件的获取
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Cacheable(value = "loginUser" ,key="#empid")
     @Override
     public MEmpDtl getUserInfo(String empid) {
         try {
+            logger.info("取得用户信息 开始");
             return empDtlMapper.getUserInfoById(empid);
         } catch (Exception ex){
             logger.error(ex.getMessage(), ex);
